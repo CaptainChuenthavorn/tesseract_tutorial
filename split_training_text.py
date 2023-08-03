75a362174a19fc0838218b632d17db8a8e8ed236
Captain <38762000+CaptainChuenthavorn@users.noreply.github.com>
2023-08-03 21:18:51 +0700

    commit

--- split_training_text.py
+++ split_training_text.py
@@ -3,15 +3,15 @@ import random
 import pathlib
 import subprocess
 
-training_text_file = 'langdata/eng.training_text'
+training_text_file = 'langdata/tha.training_text'
 
 lines = []
 
-with open(training_text_file, 'r') as input_file:
+with open(training_text_file, 'r',encoding='utf-8') as input_file:
     for line in input_file.readlines():
         lines.append(line.strip())
 
-output_directory = 'tesstrain/data/Apex-ground-truth'
+output_directory = 'tesstrain/data/Prompt-ground-truth'
 
 if not os.path.exists(output_directory):
     os.mkdir(output_directory)
@@ -26,14 +26,14 @@ line_count = 0
 for line in lines:
     training_text_file_name = pathlib.Path(training_text_file).stem
     line_training_text = os.path.join(output_directory, f'{training_text_file_name}_{line_count}.gt.txt')
-    with open(line_training_text, 'w') as output_file:
+    with open(line_training_text, 'w',encoding='utf-8') as output_file:
         output_file.writelines([line])
 
-    file_base_name = f'eng_{line_count}'
+    file_base_name = f'tha_{line_count}'
 
     subprocess.run([
         'text2image',
-        '--font=Apex',
+        '--font=Prompt',
         f'--text={line_training_text}',
         f'--outputbase={output_directory}/{file_base_name}',
         '--max_pages=1',
@@ -43,7 +43,7 @@ for line in lines:
         '--ysize=480',
         '--char_spacing=1.0',
         '--exposure=0',
-        '--unicharset_file=langdata/eng.unicharset'
+        '--unicharset_file=langdata/Thai.unicharset'
     ])
 
     line_count += 1
